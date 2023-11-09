7279e850344079b11474b44b6588d8b972b71f89
Geeth <induwarakwg3573@gmail.com>
2023-11-09 23:21:35 +0530

    Update form styling in App.css

--- client/todolist/src/App.js
+++ client/todolist/src/App.js
@@ -2,6 +2,10 @@
 import './App.css';
 
 function App() {
+
+//add new todo item to database
+
+
   return (
     <div className="App">
       <h1>Todo List</h1>
@@ -11,38 +15,38 @@ function App() {
       </form>
       <div className="todo-list-items">
         <div className="todo-item">
-          <p classname="item-content">This is the item 1</p>
-          <button classname="update-item
+          <p className="item-content">This is the item 1</p>
+          <button className="update-item
           ">Update</button>
-          <button>Delete</button>
+          <button className='delete-item'>Delete</button>
         </div>
 
         <div className="todo-item">
-          <p classname="item-content">This is the item 2</p>
-          <button classname="update-Item
+          <p className="item-content">This is the item 2</p>
+          <button className="update-item
           ">Update</button>
-          <button>Delete</button>
+          <button className="delete-item">Delete</button>
         </div>
 
         <div className="todo-item">
-          <p classname="item-content">This is the item 3</p>
-          <button classname="update-item
+          <p className="item-content">This is the item 3</p>
+          <button className="update-item
           ">Update</button>
-          <button>Delete</button>
+          <button className="delete-item">Delete</button>
         </div>
 
         <div className="todo-item">
-          <p classname="item-content">This is the item 4</p>
-          <button classname="update-item
+          <p className="item-content">This is the item 4</p>
+          <button className="update-item
           ">Update</button>
-          <button>Delete</button>
+          <button className="delete-item">Delete</button>
         </div>
 
         <div className="todo-item">
-          <p classname="item-content">This is the item 5</p>
-          <button classname="update-item
+          <p className="item-content">This is the item 5</p>
+          <button className="update-item
           ">Update</button>
-          <button>Delete</button>
+          <button className="delete-item">Delete</button>
         </div>
       </div>
     </div>
