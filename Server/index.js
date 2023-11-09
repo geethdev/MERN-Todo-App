5fcbba4ae4e5b992336c75c57ba594923f80fba1
Geeth <induwarakwg3573@gmail.com>
2023-11-09 23:46:06 +0530

    Add dependencies and update styles

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,16 @@
 const express = require ('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 //use express.json() to get data into json format
 app.use(express.json()); 
 //port
 const PORT = process.env.PORT || 5500;
+
+//use cors
+app.use(cors());
 
 
 //lets import routes
--- client/todolist/src/App.js
+++ client/todolist/src/App.js
@@ -1,16 +1,27 @@
-
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+const [itemText, setItemText ] = useState('')
+
 
 //add new todo item to database
-
+const addItem = async (e) => {
+  e.preventDefault();
+  try {
+    const res = await axios.post('http://localhost:5500/api/item',{item: itemText})
+    console.log(res);
+  }catch(err) {
+    console.error(err);
+  }
+}
 
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <form action="" className='form'>
-        <input type="text" placeholder="Enter a task" />
+      <form action="" className='form' onSubmit={e => addItem(e)}>
+        <input type="text" placeholder="Add Todo Item" onChange={e => {setItemText(e.target.value)}} value={itemText} />
         <button type="submit">Add </button>
       </form>
       <div className="todo-list-items">
